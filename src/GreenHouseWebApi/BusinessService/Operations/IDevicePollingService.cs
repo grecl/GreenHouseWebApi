acbef615afba5dd62d6437e08f7ece9116516c09
Claudio Greuter <claudio.greuter@sonova.com>
2016-12-21 14:19:40 +0100

    Added Pollcontroller that uses an internal service to trigger the device polls

---
+++ src/GreenHouseWebApi/BusinessService/Operations/IDevicePollingService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GreenHouseWebApi.BusinessService.Operations
+{
+    public interface IDevicePollingService
+    {
+        void PollDevice();
+    }
+}
---
+++ src/GreenHouseWebApi/Controllers/PollController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GreenHouseWebApi.BusinessService.Operations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenHouseWebApi.Controllers
+{
+    [Route("api/poll/{deviceId:int}")]
+    public class PollController : Controller
+    {
+        private readonly IDevicePollingService _devicePollingService;
+
+        public PollController(IDevicePollingService devicePollingService)
+        {
+            _devicePollingService = devicePollingService;
+        }
+
+        [HttpPost]
+        public IActionResult ExecuteDevicePolling(int deviceId)
+        {
+            //Todo: poll the device with the id
+            _devicePollingService.PollDevice();
+
+            return Ok();
+        }
+    }
+}
